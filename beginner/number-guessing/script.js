5f19dba7dff84c2354a65a4d99aa6415468bddc8
irfan-ahmed24 <irfanahmed99989@gmail.com>
2025-05-28 23:42:20 +0600

    guessing game

--- beginner/number-guessing/script.js
+++ beginner/number-guessing/script.js
@@ -1,4 +1,38 @@
 var number=document.querySelector("#number")
 var btn=document.querySelector(".btn")
-console.log(number);
-console.log(btn);
+var win=document.querySelector(".win")
+var choice=document.querySelector(".choice")
+var numberWas=document.querySelector(".number")
+var btn2=document.querySelector(".btn-2")
+
+
+
+btn2.addEventListener("click",function(e){
+    btn.disabled = false;
+    win.innerHTML = "";
+    choice.innerHTML = "";
+    numberWas.innerHTML = "";
+    e.preventDefault();
+    btn2.style.display = "none";
+})
+
+btn.addEventListener("click",function(e){
+    btn.disabled = "true";
+    e.preventDefault(); 
+    var guess = parseInt(number.value);
+    var randomNumber=Math.floor(Math.random()*10) + 1;
+    if(guess === randomNumber) {
+        win.innerHTML = "Congratulations! You guessed the number!";
+        choice.innerHTML = "The number was: " + randomNumber;
+        btn2.innerHTML = "new game";
+        btn2.style.display = "block";
+    }
+    else {
+        win.innerHTML = "Sorry, try again!";
+        choice.innerHTML = "Your guess: " + guess;
+        numberWas.innerHTML = "The number was: " + randomNumber;
+        btn2.innerHTML = "Try again";
+        btn2.style.display = "block";
+    }
+    number.value = "";
+})
