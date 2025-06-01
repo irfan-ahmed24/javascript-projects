f5c1d65d2e91b9fc6a86f64aae1b7c3e3dbdadfd
irfan-ahmed24 <irfanahmed99989@gmail.com>
2025-06-01 15:44:46 +0600

    toss coin

--- beginner/coin toss/script.js
+++ beginner/coin toss/script.js
@@ -2,9 +2,26 @@
 let CoinToss =document.querySelector(".Coin");
 let flipCoin=document.querySelector(".btn-1");
 let resetCoin=document.querySelector(".btn-2");
-
+resetCoin.disabled = true;
 let headsCount = 0;
 let tailsCount = 0;
+
+resetCoin.addEventListener("click",()=>{
+    CoinToss.style.animation="none";
+    headsCount = 0;
+    tailsCount = 0;
+    document.querySelector("#heads-count").textContent = `Heads: ${""}`;
+    document.querySelector("#tails-count").textContent = `Tails: ${""}`;
+    resetCoin.disabled = true;
+})
+
+const displayCount = () => {
+    setTimeout(() => {
+        document.querySelector("#heads-count").textContent = `Heads: ${headsCount}`;
+        document.querySelector("#tails-count").textContent = `Tails: ${tailsCount}`;
+    }, 3000);
+    
+}
 
 flipCoin.addEventListener("click",()=>{
     let randomNumber=Math.floor(Math.random()*2);
@@ -23,4 +40,6 @@ flipCoin.addEventListener("click",()=>{
         }, 100);
         
     }
+    displayCount();
+    resetCoin.disabled = false;
 })
